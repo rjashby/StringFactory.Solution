2f4c7819fd713f526aa6fcf69f511f4275925129
Ryan Ashby <rjapsu160@gmail.com>
2022-03-18 14:21:52 -0700

    add more styling/required text field/both.cshtml

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,12 +1,30 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using StringFactory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace StringFactory.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly StringFactoryContext _db;
+      public HomeController(StringFactoryContext db)
+      {
+        _db = db;
+      }
       [HttpGet("/")]
       public ActionResult Index()
       {
+        return View();
+      }
+
+      [HttpGet("/Both")]
+      public ActionResult Both()
+      {
+        var workerguys = _db.Engineers.ToList();
+        var fixablestuff = _db.Machines.ToList();
         return View();
       }
     }
